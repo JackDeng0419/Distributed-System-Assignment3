831d47de3f2bc84b17820b6b844119d50fbfd093
JackNeedFocus <Jack.z.deng@outlook.com>
2022-11-05 21:46:56 +1030

    change the never waiting time

--- Constant.java
+++ Constant.java
@@ -3,6 +3,6 @@ public class Constant {
     public static int PROFILE_IMMEDIATE = 0;
     public static int PROFILE_MEDIUM = 500;
     public static int PROFILE_LATE = 3000;
-    public static int PROFILE_NEVER = 9999 * 1000;
+    public static int PROFILE_NEVER = 10 * 1000;
 
 }
