6ac0032601ff88ee4f304b350f9d2f63995158e4
JackNeedFocus <Jack.z.deng@outlook.com>
2022-11-05 23:19:57 +1030

    update the comments

--- ConfigurationUtils.java
+++ ConfigurationUtils.java
@@ -10,48 +10,8 @@ import java.util.Scanner;
 
 public class ConfigurationUtils {
 
-    private final String UrlLearnerFilename = "URL_learner.txt";
-    private final String UrlProposerFilename = "URL_proposer.txt";
-    private final String UrlAcceptorFilename = "URL_accepter.txt";
-
-    private Map<String, String> urlLearnerMap = new HashMap<>();
-    private Map<String, String> urlProposerMap = new HashMap<>();
-    private Map<String, String> urlAccepterMap = new HashMap<>();
-
     public static HashMap<String, AccepterInfo> accepterMap = new HashMap<>();
     public static HashMap<String, ProposerInfo> proposerMap = new HashMap<>();
-
-    /*
-     * This constructor reads the URL list file and construct the maps
-     */
-    public ConfigurationUtils() {
-        Scanner scanner;
-        try {
-            // construct the learner map
-            scanner = new Scanner(new File(UrlLearnerFilename));
-            while (scanner.hasNextLine()) {
-                String[] strings = scanner.nextLine().split("-", 2);
-                urlLearnerMap.put(strings[0], strings[1]);
-            }
-
-            // construct the proposer map
-            scanner = new Scanner(new File(UrlProposerFilename));
-            while (scanner.hasNextLine()) {
-                String[] strings = scanner.nextLine().split("-", 2);
-                urlProposerMap.put(strings[0], strings[1]);
-            }
-
-            // construct the acceptor map
-            scanner = new Scanner(new File(UrlAcceptorFilename));
-            while (scanner.hasNextLine()) {
-                String[] strings = scanner.nextLine().split("-", 2);
-                urlAccepterMap.put(strings[0], strings[1]);
-            }
-        } catch (FileNotFoundException e) {
-            System.out.println("URLList failed to construct.");
-            e.printStackTrace();
-        }
-    }
 
     public static void ConfigurationInit(String configFilename) {
         Scanner scanner;
@@ -100,38 +60,10 @@ public class ConfigurationUtils {
                 }
             }
 
-            // // construct the proposer map
-            // scanner = new Scanner(new File(UrlProposerFilename));
-            // while (scanner.hasNextLine()) {
-            // String[] strings = scanner.nextLine().split("-", 2);
-            // urlProposerMap.put(strings[0], strings[1]);
-            // }
-
-            // // construct the acceptor map
-            // scanner = new Scanner(new File(UrlAcceptorFilename));
-            // while (scanner.hasNextLine()) {
-            // String[] strings = scanner.nextLine().split("-", 2);
-            // urlAccepterMap.put(strings[0], strings[1]);
-            // }
         } catch (FileNotFoundException e) {
             System.out.println("Configuration failed.");
             e.printStackTrace();
         }
     }
 
-    public Map<String, String> getUrlLearnerMap() {
-        return urlLearnerMap;
-    }
-
-    public Map<String, String> getUrlProposerMap() {
-        return urlProposerMap;
-    }
-
-    public Map<String, String> getUrlAccepterMap() {
-        return urlAccepterMap;
-    }
-
-    public int getAccepterCount() {
-        return urlAccepterMap.size();
-    }
 }
